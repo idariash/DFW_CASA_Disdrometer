a595deccadd52a28ff85ceafc6cda5ca0d773c07
Ivan Arias Hernandez <idariash@colostate.edu>
2024-02-13 15:35:03 -0700

    Making a function-like library

--- code/disdrometer_analysis.py
+++ code/disdrometer_analysis.py
@@ -27,50 +27,57 @@ from matplotlib.dates import DateFormatter
 import pytz
 import matplotlib.colors as mcolors
 
-Data_Path = '/net/k2/storage/people/idariash/home/CSU/DFW/data/Charlie/20240108'
-'/net/k2/storage/people/idariash/home/CSU/DFW/data/20240108'
-
-
-
-
-files = os.listdir(Data_Path)
-files = sorted(files)
-
-Bin_Size = np.empty((0, 32))
-Bin_Counts = np.empty((0, 32))
-Times = []
-
-for file in files:
-    filename = os.path.join(Data_Path, file)
-    dsd = pyd.read_parsivel(filename)
-    dsd.calculate_dsd_parameterization()
-    bin_size = dsd.diameter['data']
-    bin_counts = dsd.fields['Nd']['data'][0]
-    time = datetime.datetime.fromtimestamp(dsd.time['data'][0]) 
-    time = time + timedelta(hours=1)
+def plot_time_series(Data_Path):
     
-    Bin_Size = np.append(Bin_Size, [bin_size], axis=0)
-    Bin_Counts = np.append(Bin_Counts, [bin_counts], axis=0)
-    Times.append(time)
-    #formatted_dates = [date.strftime("%M:%S") for date in Times]
+    files = os.listdir(Data_Path)
+    files = sorted(files)
     
+    Bin_Size = np.empty((0, 32))
+    Bin_Counts = np.empty((0, 32))
+    Times = []
+    
+    for file in files:
+        filename = os.path.join(Data_Path, file)
+        dsd = pyd.read_parsivel(filename)
+        dsd.calculate_dsd_parameterization()
+        bin_size = dsd.diameter['data']
+        bin_counts = dsd.fields['Nd']['data'][0]
+        time = datetime.datetime.fromtimestamp(dsd.time['data'][0]) 
+        time = time + timedelta(hours=1) # adding an hour to match central time
+        
+        Bin_Size = np.append(Bin_Size, [bin_size], axis=0)
+        Bin_Counts = np.append(Bin_Counts, [bin_counts], axis=0)
+        Times.append(time)
+        #formatted_dates = [date.strftime("%M:%S") for date in Times]
+        
+    fig = plt.figure(figsize = [6,4])
+    colors = [("white")] + [(cm.jet(i)) for i in range(1, 256)]
+    cmap = mpl.colors.LinearSegmentedColormap.from_list("new_map", colors, N=256)
+    plt.pcolormesh(Times, bin_size, Bin_Counts.T, cmap = cmap,
+                         norm=mcolors.LogNorm(vmin=1, vmax=1E4))
+    plt.axis("tight")
+    plt.ylim(0, 7)
+    hh_mm = DateFormatter('%H:%M')
+    plt.gca().xaxis.set_major_formatter(hh_mm)
+    
+    plt.xlabel('Time (Central)')
+    plt.ylabel('Diameter (mm)')
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel('Counts (mm^-1 m^-3)')
+    
+    # Set limits for the color bar
+    cbar.set_clim(1E0, 1E4)
+    
+    return fig
 
-colors = [("white")] + [(cm.jet(i)) for i in range(1, 256)]
-cmap = mpl.colors.LinearSegmentedColormap.from_list("new_map", colors, N=256)
-plt.pcolormesh(Times, bin_size, Bin_Counts.T, cmap = cmap,
-                     norm=mcolors.LogNorm(vmin=1, vmax=1E4))
-plt.axis("tight")
-plt.ylim(0, 7)
-hh_mm = DateFormatter('%H:%M')
-plt.gca().xaxis.set_major_formatter(hh_mm)
+def main():
 
-plt.xlabel('Time (Central)')
-plt.ylabel('Diameter (mm)')
-cbar = plt.colorbar()
-cbar.ax.set_ylabel('Counts (mm^-1 m^-3)')
-
-# Set limits for the color bar
-cbar.set_clim(1E0, 1E4)
+    Data_Path = '/net/k2/storage/people/idariash/home/CSU/DFW/data/Charlie/20240108'
+    '/net/k2/storage/people/idariash/home/CSU/DFW/data/20240108'
+    plot_time_series(Data_Path)
+    
+if __name__ == "__main__":
+    main()
 
 
 
